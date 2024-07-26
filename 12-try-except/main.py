9a1e60733f274d6b17cf3b6f97cbaf8d94e025ce
Shadow8021 <gregoyaga@gmail.com>
2024-07-26 11:40:37 +0100

    raise function

--- 12-try-except/main.py
+++ 12-try-except/main.py
@@ -1,10 +1,13 @@
-
+user =int(input("veuillez entrez un nombre:"))
 b=20
+if user:
+    raise ValueError
+print("fin")
 try:
-    user =int(input("veuillez entrez un nombre:"))
     c=b/user
     print(f"Le resultat est : {c}")
 except ZeroDivisionError as Err:
     print(Err)
 except ValueError as Err:
     print(Err)
+
