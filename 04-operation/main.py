0506cdd3f4fd36a6662bf01f3546d8cc1acec0ae
Shadow8021 <gregoyaga@gmail.com>
2026-07-06 10:39:02 +0100

    concatenation

---
+++ 04-operation/main.py
@@ -0,0 +1,25 @@
+#les opérateurs sont des symbolsqui permettent de faire des calcules et des varifications
+#Operatuet arithmetique
+    #Ils permettent de faire des caculer comme addition,soustraction, division,...
+# addition +
+add = 3+2
+print(add) #add=5
+
+# division /
+div = 10/2
+print(div) #div=5
+
+# soustraction -
+sous = 3-2
+print(sous) #sous=1
+
+# multiplication *
+mult = 3*2
+print(mult) #mult=6
+
+# le modulo %
+#cette operateur permet de trouver le reste d'une division
+modul = 4%2
+print(modul) #modul=0
+
+
