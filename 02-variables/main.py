eed51a5eb42276d9ffe4a40bee16499bda665d83
Shadow8021 <gregoyaga@gmail.com>
2026-07-06 09:36:29 +0100

    notion de variable

---
+++ 02-variables/main.py
@@ -0,0 +1,15 @@
+#les variables representent des conteneur qui permettent de stoker des données utilisable par le programme 
+#chaque variable est liée à un type de donnée (integer,string,boolean,float)
+
+#"integer" ou encore "int" représente un nombre entier . Exemple: 20; 15 ;48  
+age = 20  #integer 
+#"string" ou encore "str" représente une chaine de caractères . Exemple: "bonjour"; "bonjour jelien comment vas-tu?" ;"56" 
+#En python tout ce qui est écrit dans les guillemet est une chaine de caracter. Exemple: "56"; "école"
+nom="hacker"
+date="2026-7-6"
+#"Boolean" ou encore "boll" ne peut representé que deux valeur soit VRAI soit FAUX
+est_admin=True
+est_admin=False
+#"float" servent à représenté des nombres decimaux . Exemple: 15.26; 225.62 ;100.00 
+prix=12.23
+moyenne=15.45
