8c196966e52c6c94940612fc37a69fd95af86e6d
Shadow8021 <gregoyaga@gmail.com>
2026-07-06 09:52:53 +0100

    concatenation

--- 02-variables/main.py
+++ 02-variables/main.py
@@ -3,13 +3,29 @@
 
 #"integer" ou encore "int" représente un nombre entier . Exemple: 20; 15 ;48  
 age = 20  #integer 
+print(age)
+
 #"string" ou encore "str" représente une chaine de caractères . Exemple: "bonjour"; "bonjour jelien comment vas-tu?" ;"56" 
 #En python tout ce qui est écrit dans les guillemet est une chaine de caracter. Exemple: "56"; "école"
 nom="hacker"
+print(nom)
+
 date="2026-7-6"
+print(date)
+
 #"Boolean" ou encore "boll" ne peut representé que deux valeur soit VRAI soit FAUX
 est_admin=True
-est_admin=False
+print(est_admin)
+
+est_etudiant=False
+print(est_etudiant)
+
 #"float" servent à représenté des nombres decimaux . Exemple: 15.26; 225.62 ;100.00 
 prix=12.23
+print(prix)
+
 moyenne=15.45
+print(moyenne)
+
+#pour afficher le contenue d'une variable on utilise la fonction print et en passe le nom de la variable en paramètre
+
