f26f16015f9534cbb02327e69c4a1fb4e3ac05f4
Shadow8021 <gregoyaga@gmail.com>
2026-07-07 09:43:29 +0100

    concatenation

---
+++ 03-concatenation/main.py
@@ -0,0 +1,8 @@
+#la concatenation est un processus qui permet d'associer deux ou plusieur chaine de caractère
+print("bonjour"+" toto")
+
+#RESULTAT: "bonjour toto"
+#Elle est souvent utilé pour dynamiser l'affichage des informations
+
+nom="toto"
+print("bonjour " + nom)
