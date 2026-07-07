865bd99244bc6876cb4932a9a515725e10642b81
Shadow8021 <gregoyaga@gmail.com>
2026-07-07 10:41:46 +0100

    condition

--- 04-condition/main.py
+++ 04-condition/main.py
@@ -1,2 +1,1 @@
-#En programmation tout comme de maniere globale, les conditions servent à faire des verification ou les analyse des cas
-#Pour créer une condition en python en utlise le mot "if"
+#les condition permette de structure l'execution du programe de sorte que certains ne n'execute que si certaines condition sans respectées
