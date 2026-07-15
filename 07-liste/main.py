7454fa8d256f2c2422a4505d476fb38f7ff4b9fd
Shadow8021 <gregoyaga@gmail.com>
2026-07-15 11:56:29 +0100

    slicing methode

--- 07-liste/main.py
+++ 07-liste/main.py
@@ -17,7 +17,18 @@ print(list_eleves[-3]) #Charlie
 print(list_eleves[-2]) #David
 print(list_eleves[-1]) #Eva
 
-#methode d'une liste
+###methode d'une liste
 
 print(len(list_eleves)) #len permet de compter le nombre d'element de la liste
 
+    #slicing 
+print(list_eleves[1:4]) #affiche les elements d'index 1, 2 et 3
+print(list_eleves[:3]) #affiche les elements d'index 0, 1 et 2
+print(list_eleves[2:5]) #affiche les elements d'index 2, 3 et 4
+print(list_eleves[1:5:2]) #affiche les elements d'index 1, 3 et 5
+
+"""
+jdjdk
+dlddl
+dldd
+"""
