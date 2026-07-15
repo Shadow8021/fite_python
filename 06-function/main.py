6d7a112c07049df19ffbc396869f032a69231ca6
Shadow8021 <gregoyaga@gmail.com>
2026-07-15 10:44:19 +0100

    jour 5

---
+++ 06-function/main.py
@@ -0,0 +1,16 @@
+#ils stocke une logique de code reutilisable
+
+def Bonjour () :
+    print("bonjour")
+
+
+#appelle de la fonction
+Bonjour()
+
+
+#les paramètres sont des valeur que l'on peut donnée à la fonction pour etre traité
+
+def Bonjour (nom) :
+    print("bonjour",nom)
+
+Bonjour("martial")
