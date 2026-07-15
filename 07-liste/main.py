0fc96fb6097a0d077ad2a8dfa84edd9d68df52f9
Shadow8021 <gregoyaga@gmail.com>
2026-07-15 11:45:14 +0100

    affichage et methode

--- 07-liste/main.py
+++ 07-liste/main.py
@@ -1,2 +1,13 @@
+#creation d'une liste
 list_eleves=["Alice", "Bob", "Charlie", "David", "Eva"]
+#affichage de la liste
 print(list_eleves)
+
+#afficher un élement en fonction de son index
+print(list_eleves[4])
+
+
+#methode d'une liste
+
+print(len(list_eleves)) #len permet de compter le nombre d'element de la liste
+
