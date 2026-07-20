956df5f83685b20a4ccff7a1c86ff40c401c138c
Shadow8021 <gregoyaga@gmail.com>
2026-07-20 11:57:26 +0100

    object state

---
+++ 08-objet/main.py
@@ -0,0 +1,15 @@
+student ={
+    "id": 1,
+    "name": "Nko",
+    "last_name": "Foa",
+    "age": 22,
+    "grade": 15.5,
+    "subjects": ["Math", "Physics", "English"],
+    "address": {
+        "street": "123 Main St",
+        "city": "Dakar",
+        "country": "Senegal"
+    },
+    "active": True,
+    "scores":{"match":[15,17,14],"physics":[16,15,18],"english":[12,14,13]}
+}
