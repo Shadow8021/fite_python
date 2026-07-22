3dbd816378ee0392e97065170aaf0423001e75f7
Shadow8021 <gregoyaga@gmail.com>
2026-07-22 11:14:52 +0100

    union

---
+++ 10-insserstion/main.py
@@ -0,0 +1,6 @@
+group_python={'Amina','Brice','Celine'}
+group_web={'Brice','David','Amina'}
+#union
+print(group_python | group_web)
+print(group_python & group_web)
+print(group_python - group_web)
