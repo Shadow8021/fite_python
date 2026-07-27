804476bbd109d42a35f8053d4f8bcb77304be7fe
Shadow8021 <gregoyaga@gmail.com>
2026-07-27 10:42:40 +0100

    csv

--- 11-fichier/main.py
+++ 11-fichier/main.py
@@ -1,5 +1,18 @@
+import csv
 with open('11-fichier/main.txt','r') as f:
     contenu = f.readline()
     
 for i in contenu:
     print(f"-> {i.strip()}")
+
+
+with open('11-fichier/main.csv','r') as f:
+    contenu = csv.DictReader(f)
+    eligible=[
+        row for row in contenu if row.get("Bonus Decision")=="Eligible"
+    ]
+
+with open('11-fichier/main.csv','w', newline="") as f:
+    writer = csv.DictWriter(f,fieldnames=eligible[0].keys)
+    writer.writeheader()
+    writer.writerows(eligible)
