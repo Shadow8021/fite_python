2075a24ae02d764edb68c608af54385c1b02ab54
Shadow8021 <gregoyaga@gmail.com>
2026-07-27 11:25:07 +0100

    test valueError

--- 12-try-except/main.py
+++ 12-try-except/main.py
@@ -1,7 +1,10 @@
-user =int(input("veuillez entrez un nombre:"))
+
 b=20
 try:
+    user =int(input("veuillez entrez un nombre:"))
     c=b/user
     print(f"Le resultat est : {c}")
 except ZeroDivisionError as Err:
+    print(Err)
+except ValueError as Err:
     print(Err)
