ddde05f0b6be04880ca8cffe5a15bdee1881a58f
Shadow8021 <gregoyaga@gmail.com>
2026-08-10 11:29:04 +0100

    end exo

--- 14-tkinter/main.py
+++ 14-tkinter/main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 
 fenetre = tk.Tk()
 fenetre.title("Formulaire")
-fenetre.geometry("100x300")
+fenetre.geometry("500x300")
 
 formulaire = tk.Frame(fenetre)
 formulaire.pack(pady=20)
@@ -14,6 +14,7 @@ tk.Label(formulaire, text="Nom :").grid(
 tk.Entry(formulaire).grid(
     row=0, column=1, padx=10, pady=10
 )
+
 
 tk.Label(formulaire, text="Prénoms :").grid(
     row=1, column=0, padx=10, pady=10
@@ -31,11 +32,6 @@ tk.Entry(formulaire).grid(
     row=2, column=1, padx=10, pady=10
 )
 
-
-# =====================================
-# FRAME BUTTONS
-# =====================================
-
 buttons = tk.Frame(fenetre)
 buttons.pack(pady=10)
 
@@ -50,6 +46,7 @@ tk.Button(buttons, text="Effacer").pack(
 tk.Button(buttons, text="Quitter").pack(
     side="left", padx=5
 )
+
 
 
 fenetre.mainloop()
