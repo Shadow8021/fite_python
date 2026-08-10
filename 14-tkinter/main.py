36622f68e1b892e42826ff37cd88b8de51fcaa84
Shadow8021 <gregoyaga@gmail.com>
2026-08-10 10:55:56 +0100

    first exercice

--- 14-tkinter/main.py
+++ 14-tkinter/main.py
@@ -1,5 +1,55 @@
 import tkinter as tk
-fenetre =tk.Tk()
+
+fenetre = tk.Tk()
 fenetre.title("Formulaire")
+fenetre.geometry("500x300")
+
+formulaire = tk.Frame(fenetre)
+formulaire.pack(pady=20)
+
+tk.Label(formulaire, text="Nom :").grid(
+    row=0, column=0, padx=10, pady=10
+)
+
+tk.Entry(formulaire).grid(
+    row=0, column=1, padx=10, pady=10
+)
+
+tk.Label(formulaire, text="Prénoms :").grid(
+    row=1, column=0, padx=10, pady=10
+)
+
+tk.Entry(formulaire).grid(
+    row=1, column=1, padx=10, pady=10
+)
+
+tk.Label(formulaire, text="Ville :").grid(
+    row=2, column=0, padx=10, pady=10
+)
+
+tk.Entry(formulaire).grid(
+    row=2, column=1, padx=10, pady=10
+)
+
+
+# =====================================
+# FRAME BUTTONS
+# =====================================
+
+buttons = tk.Frame(fenetre)
+buttons.pack(pady=10)
+
+tk.Button(buttons, text="Valider").pack(
+    side="left", padx=5
+)
+
+tk.Button(buttons, text="Effacer").pack(
+    side="left", padx=5
+)
+
+tk.Button(buttons, text="Quitter").pack(
+    side="left", padx=5
+)
+
 
 fenetre.mainloop()
