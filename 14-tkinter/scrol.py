f6ce0c26d606315249eccc173489b4f70bb74a04
Shadow8021 <gregoyaga@gmail.com>
2026-08-12 12:46:09 +0100

    fin

--- 14-tkinter/scrol.py
+++ 14-tkinter/scrol.py
@@ -2,4 +2,35 @@ import tkinter as tk
 
 fen=tk.Tk()
 fen.title("test scrollbar")
+list=tk.Listbox(fen)
+list.pack(side="left", fill="y")
+barre=tk.Scrollbar(fen,orient='vertical',command=list.yview)
+barre.pack(side="right",fill='y')
+list.config(yscrollcommand=barre.set)
+for i in range(80):
+    list.insert(tk.END,f"Element{i} Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur, ")
+
+
+list2=tk.Listbox(fen)
+barre2=tk.Scrollbar(fen,orient='horizontal',command=list2.yview)
+barre2.pack(side="bottom",fill='y')
+list2.config(yscrollcommand=barre2.set)
+for i in range(50):
+    list2.insert(tk.END,f"Element{i} Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur, ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 fen.mainloop()
