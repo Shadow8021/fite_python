e1aea81d92411e76484b1d40dacd747054dfa237
Shadow8021 <gregoyaga@gmail.com>
2026-08-12 11:08:21 +0100

    1 menu fonctionnel

--- 14-tkinter/manu.py
+++ 14-tkinter/manu.py
@@ -6,9 +6,11 @@ fenetre.geometry("500x500")
 #menu
 menu_principal=tk.Menu(fenetre)
 fenetre.config(menu=menu_principal)
-menu_fichier=tk.Menu(menu_principal,tearoff=0)
+menu_fichier=tk.Menu(menu_principal)
 menu_principal.add_cascade(label="Fichier",menu=menu_fichier)
 menu_fichier.add_command(label="Nouveau",command=lambda: print("Nouveau"))
+menu_fichier.add_command(label="Ouvrir",command=lambda: print("Ouvrir"))
+menu_fichier.add_command(label="Quitter")
 
 
 
