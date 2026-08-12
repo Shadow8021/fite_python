e8cd8f55eafb9450e9980786c8108e87997fe162
Shadow8021 <gregoyaga@gmail.com>
2026-08-12 11:02:08 +0100

    menu principal creation

--- 14-tkinter/manu.py
+++ 14-tkinter/manu.py
@@ -3,6 +3,22 @@ import tkinter as tk
 fenetre=tk.Tk()
 fenetre.title("mon menu")
 fenetre.geometry("500x500")
+#menu
 menu_principal=tk.Menu(fenetre)
 menu_fichier=tk.Menu(menu_principal,tearoff=0)
+menu_principal.add_cascade(label="Fichier",menu=menu_fichier)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 fenetre.mainloop()
