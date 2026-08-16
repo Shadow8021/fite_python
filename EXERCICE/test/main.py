fc9bae52c8fb9b2d55fb0818a3650c6a22f6bb42
Shadow8021 <gregoyaga@gmail.com>
2026-08-16 22:31:25 +0100

    button

--- EXERCICE/test/main.py
+++ EXERCICE/test/main.py
@@ -4,8 +4,8 @@ fenetre = tk.Tk()
 fenetre.geometry("700x300")
 
 def onClick():
-    label=tk.Label(fenetre, text="you have clicked on the button")
+    label=tk.Label(fenetre, text="you have clicked on the button", fg='green')
     label.pack(side="left",padx=100)
 
-btn=tk.Button(fenetre,text="click me", padx=10, pady=50, command=onClick).pack(side="left",padx=40)
+btn=tk.Button(fenetre,text="click me", padx=10, pady=50, command=onClick,fg="blue").pack(side="left",padx=40)
 fenetre.mainloop()
