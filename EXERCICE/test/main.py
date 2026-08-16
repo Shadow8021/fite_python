c6e5bb8edd2486850aa1ff1c68b206eb4364bc2e
Shadow8021 <gregoyaga@gmail.com>
2026-08-16 22:22:06 +0100

    button

--- EXERCICE/test/main.py
+++ EXERCICE/test/main.py
@@ -3,5 +3,5 @@ import tkinter as tk
 fenetre = tk.Tk()
 fenetre.geometry("700x300")
 
-btn=tk.Button()
+btn=tk.Button(fenetre,text="click me", padx=10, pady=50).pack(side="left",padx=40)
 fenetre.mainloop()
