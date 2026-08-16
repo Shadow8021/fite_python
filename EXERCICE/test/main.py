065ab693036b6f78370fd58574d3c00ba78c51cc
Shadow8021 <gregoyaga@gmail.com>
2026-08-16 22:19:51 +0100

    button

--- EXERCICE/test/main.py
+++ EXERCICE/test/main.py
@@ -3,23 +3,5 @@ import tkinter as tk
 fenetre = tk.Tk()
 fenetre.geometry("700x300")
 
-label = tk.Label(
-    fenetre,
-    text="Bonjour Tkinter",
-    font=("Liberation Sans", 10, "bold italic")
-).grid(column=0,row=0)
-
-label1 = tk.Label(
-    fenetre,
-    text="Bonjour Tkinter",
-    font=("Liberation Sans", 10, "bold italic")
-).pack(side="bottom", padx=40)
-
-label = tk.Label(
-    fenetre,
-    text="Bonjour Tkinter",
-    font=("Liberation Sans", 10, "bold italic")
-).place(x=10,y=10)
-
-
+btn=tk.Button()
 fenetre.mainloop()
