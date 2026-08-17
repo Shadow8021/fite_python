922c82f182dc82acbf977df56b99cf9b2bdb826a
Shadow8021 <gregoyaga@gmail.com>
2026-08-17 23:34:55 +0100

    calculatrice

--- EXERCICE/test/main.py
+++ EXERCICE/test/main.py
@@ -3,13 +3,36 @@ import tkinter as tk
 fenetre = tk.Tk()
 fenetre.geometry("230x300")
 fenetre.resizable(width=False,height=False)
-a=tk.Entry(fenetre,width=100,border=3)
-a.insert(0,"martial")
-a.pack()
+a=tk.Entry(fenetre,border=3)
+a.grid(column=0,row=0)
 
 def onClick():
     lab=tk.Label(fenetre, text=f"Vous avez saisie {a.get()}")
     lab.pack()
 
-btn=tk.Button(fenetre,text="envoyer", command=onClick).place(x=0,y=50)
+btn9=tk.Button(fenetre,text="9", command=onClick)
+btn8=tk.Button(fenetre,text="8", command=onClick)
+btn7=tk.Button(fenetre,text="7", command=onClick)
+btn6=tk.Button(fenetre,text="6", command=onClick)
+btn5=tk.Button(fenetre,text="5", command=onClick)
+btn4=tk.Button(fenetre,text="4", command=onClick)
+btn3=tk.Button(fenetre,text="3", command=onClick)
+btn2=tk.Button(fenetre,text="2", command=onClick)
+btn1=tk.Button(fenetre,text="1", command=onClick)
+btn0=tk.Button(fenetre,text="0", command=onClick)
+
+
+btn9.grid(column=1,row=1)
+btn8.grid(column=0,row=1)
+btn7.grid(column=0,row=1)
+
+btn6.grid(column=1,row=2)
+btn5.grid(column=2,row=2)
+btn4.grid(column=3,row=2)
+
+btn3.grid(column=1,row=3)
+btn2.grid(column=2,row=3)
+btn1.grid(column=3,row=3)
+
+
 fenetre.mainloop()
