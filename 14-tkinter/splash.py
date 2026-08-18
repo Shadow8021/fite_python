2b5ac3b2ee0d29b1572355d20f145f21a0facd61
Shadow8021 <gregoyaga@gmail.com>
2026-08-18 21:26:26 +0100

    splash

--- 14-tkinter/splash.py
+++ 14-tkinter/splash.py
@@ -4,8 +4,8 @@ fenetre=tk.Tk()
 fenetre.withdraw()
 splash=tk.Toplevel(fenetre)
 splash.overrideredirect(True)
-splash.geometry("300x200+500+300")
-ttk.Label(splash,text="Bienvenue",bg="gree",fg="red").pack(padx=10,pady=10,fill="x")
+splash.geometry("700x400+360+160")
+ttk.Label(splash,text="Bienvenue").pack(padx=10,pady=10,fill="x")
 
 
 def demarrer():
