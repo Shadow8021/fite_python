f62ab20fc1afb04553b3921287f8ad6764e52356
Shadow8021 <gregoyaga@gmail.com>
2026-08-18 21:18:35 +0100

    splash

--- 14-tkinter/splash.py
+++ 14-tkinter/splash.py
@@ -1,9 +1,10 @@
 import tkinter as tk
+from tkinter import ttk
 fenetre=tk.Tk()
 fenetre.withdraw()
 splash=tk.Toplevel(fenetre)
 splash.overrideredirect(True)
 splash.geometry("300x200+500+300")
-
+ttk.Label(splash,text="Bienvenue",bg="gree",fg="red").pack(padx=10,pady=10,fill="x")
 
 fenetre.mainloop()
