76a2eecb5a154076adb5b89d5387960ddd035e53
Shadow8021 <gregoyaga@gmail.com>
2026-08-19 11:20:02 +0100

    creation de la table

--- 15-Bd/main.py
+++ 15-Bd/main.py
@@ -4,16 +4,23 @@ import sqlite3
 con=sqlite3.connect("./15-Bd/martial.db")
 cursor=con.cursor()
 #creation de la table
-cursor.execute("""
+#creation de plusieurs tables au meme moment
+cursor.executescript("""
     create table if not exists produits (
-        id int primary key,
-        name varchar(255) not null,
-        stock int 
-    )
-""")
+            id int primary key,
+            nom varchar(255) not null,
+            prix real
+            stock int );
 
-cursor.execute("""create table if not exists persons(
+    create table if not exists persons(
         id int primary key,
         nom varchar(255) not null,
-        prenoms varchar(255) not null
-    )""")
+        prenoms varchar(255) not null,
+        prone integer not null
+    )
+
+
+
+""")
+
+con.commit()
