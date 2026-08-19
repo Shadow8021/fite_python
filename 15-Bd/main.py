42b2d882d1a3ef39e77715d1f78b680eb130a529
Shadow8021 <gregoyaga@gmail.com>
2026-08-19 11:07:29 +0100

    creation de la table

--- 15-Bd/main.py
+++ 15-Bd/main.py
@@ -3,3 +3,17 @@ import sqlite3
 #provider
 con=sqlite3.connect("./15-Bd/martial.db")
 cursor=con.cursor()
+#creation de la table
+cursor.execute("""
+    create table if not exists produits (
+        id int primary key,
+        name varchar(255) not null,
+        stock int 
+    )
+""")
+
+cursor.execute("""create table if not exists persons(
+        id int primary key,
+        nom varchar(255) not null,
+        prenoms varchar(255) not null
+    )""")
