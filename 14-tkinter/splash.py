a9b24ab62673cf934342688ee329cf64cd8b289d
Shadow8021 <gregoyaga@gmail.com>
2026-08-20 10:52:42 +0100

    sql+tkinter

--- 14-tkinter/splash.py
+++ 14-tkinter/splash.py
@@ -5,12 +5,12 @@ fenetre.withdraw()
 splash=tk.Toplevel(fenetre)
 splash.overrideredirect(True)
 splash.geometry("700x400+360+160")
-ttk.Label(splash,text="Bienvenue").pack(padx=10,pady=10,fill="x")
+ttk.Label(splash,text="Bienvenue").place(x=300,y=100)
 
 
 def demarrer():
     splash.destroy()
     fenetre.deiconify()
 splash.after(5000, demarrer)
-
+tk.Label(fenetre,text="mon app").place(x=300,y=100)
 fenetre.mainloop()
