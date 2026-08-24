edbc3d214e1f9415ad9d27d54baf285013e1a388
Shadow8021 <gregoyaga@gmail.com>
2026-08-24 11:35:25 +0100

    add methodes

--- 15-Bd/exoPOO/main.py
+++ 15-Bd/exoPOO/main.py
@@ -22,9 +22,10 @@ class DataBase:
 
     def insert_stuent(self, datas):
         self.cursor.execute("""insert into etudiants(
-                nom,telephone,sexe,classe,photo_path,date-inscription
+                nom,telephone,sexe,classe,photo_path,date_inscription
                 ) values(?,?,?,?,?,?,?)""",(datas))
         self.con.commit()
+        self.cursor.lastrowid
 
 
     def get_All(self):
@@ -35,8 +36,12 @@ class DataBase:
         self.cursor.execute("select * from etudiants where id=?",(id))
         return (self.cursor.fetchone())
 
-    def update (self,id):
-        self.cursor.execute("update ")
-
-
-
+    def update (self,datas,id):
+        requete="""update etudiants set nom=?,
+                                    prenom=?,
+                                    telephone=?,
+                                    sexe=?,
+                                    photo_path=?
+                                    ,date_inscription where id=?"""
+        self.cursor.execute(requete,(datas,id))
+        self.con.commit()
