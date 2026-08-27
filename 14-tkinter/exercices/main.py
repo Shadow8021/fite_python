f024eb4b5fe52bab38b9c5d0ba444231f06e76a7
Shadow8021 <gregoyaga@gmail.com>
2026-08-27 22:58:50 +0100

    test image

--- 14-tkinter/exercices/main.py
+++ 14-tkinter/exercices/main.py
@@ -1,7 +1,11 @@
 import tkinter as tk
-
+from PIL import Image,ImageTk
 app=tk.Tk()
+app.title("martial")
 
+image=ImageTk.PhotoImage(Image.open("./fite.png"))
+labe=tk.Label(image=image)
+labe.pack()
 app.mainloop()
 
 
